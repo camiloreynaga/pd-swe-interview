c0c193880eb4f5dc97cf230b0f3fef5cb12adbf9
Christopher Little-Savage <christopher@littlesavage.net>
2025-01-21 20:37:39 +0000

    refine starting code

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,36 @@
 interface Reading {
-  // TODO: change this to contain whatever information is needed
+	farmerId: string;
+	attribute: string;
+	value: unknown;
+}
+
+interface Aggregate {
+	min: number;
+	max: number;
+	sum: number;
+	count: number;
+}
+
+interface Farmer {
+	// TODO: define the Farmer type
 }
 
 // This is a fake database which stores data in-memory while the process is running
 // Feel free to change the data structure to anything else you would like
-const database: Record<string, Reading> = {};
+const database: Record<string, Farmer> = {};
 
 /**
  * Store a reading in the database using the given key
  */
-export const addReading = (key: string, reading: Reading): Reading => {
-  database[key] = reading;
-  return reading;
+export const addReading = (reading: Reading): Farmer => {
+	// TODO: implement this function
+	throw new Error("Not implemented");
 };
 
 /**
  * Retrieve a reading from the database using the given key
  */
-export const getReading = (key: string): Reading | undefined => {
-  return database[key];
+export const getFarmersByCountry = (country: string): Farmer[] => {
+	// TODO: implement this function
+	throw new Error("Not implemented");
 };
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Express } from 'express';
-import helmet from 'helmet';
-import dotenv from 'dotenv';
-import { addReading, getReading } from './database';
+import dotenv from "dotenv";
+import express, { type Express } from "express";
+import helmet from "helmet";
+import { addReading, getFarmersByCountry } from "./database";
 
 dotenv.config();
 
@@ -12,22 +12,18 @@ app.use(helmet());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/data', async (req, res) => {
-  // TODO: parse incoming data, and save it to the database
-  // data is of the form:
-  //  {farmerid} {attribute} {value}
+app.post("/data", async (req, res) => {
+	// TODO: parse incoming data, and save it to the database
+	// data is of the form:
+	//  {farmerid} {attribute} {value}
 
-  // addReading(...)
-
-  return res.json({ success: false });
+	return res.json({ success: false });
 });
 
-app.get('/aggregate', async (req, res) => {
-  // TODO: check what dates have been requested, and retrieve all data within the given range
-
-  // getReading(...)
-
-  return res.json({ success: false });
+app.get("/aggregate", async (req, res) => {
+	// TODO: check what country has been requested, and return aggregates for the
+	// named attributes for all farmers in that country
+	return res.json({ success: false });
 });
 
 app.listen(PORT, () => console.log(`Running on port ${PORT} ⚡`));
